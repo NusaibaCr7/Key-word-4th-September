91dfedbc1f0e4e4eca98d0c1f275883921a79b1f
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-09-04 18:45:40 +0600

    siuuu

--- Break.py
+++ Break.py
@@ -1,1 +1,9 @@
-print("hello world")
+a = input("Enter a word: ")
+
+for i in a:
+    if i=='A' or i=='a':
+        print(f"A is found at {i}")
+        break
+    
+    else:
+        print(f"A is not found at {i}")
