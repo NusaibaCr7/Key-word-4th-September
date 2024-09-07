63b8a9a9daca246f3ac0e7a4e99bbac2b2d69797
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-09-07 18:23:26 +0600

    oi hi python mama

---
+++ Due amount.py
@@ -0,0 +1,9 @@
+def dueAmount(b,g):
+    return g-b
+
+bill = int(input("Enter the bill: "))
+given = int(input("Enter the given amount: "))
+
+res = dueAmount(bill,given)
+
+print(f"Bill: {bill}\n Given Amount: {given}\n Due Amount: {res}")
